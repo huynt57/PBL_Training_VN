5fa2625309b7588a3ecd791bcf8e5469ffdd7538
trangnt_57 <trangnt_57@vnu.edu.vn>
2014-08-30 09:48:56 +0700

    how to use ajax with json and rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'courses/show'
+
+  get 'courses/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,6 +59,4 @@ Rails.application.routes.draw do
   #   end
   root "courses#index"
   resources :courses
-  resources :students
-  resources :students_courses
 end
